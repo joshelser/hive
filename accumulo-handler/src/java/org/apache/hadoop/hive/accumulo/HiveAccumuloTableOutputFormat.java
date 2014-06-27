7ec49870bf64e17a952613db92e5b39fe020708d
Josh Elser <josh.elser@gmail.com>
2014-06-27 17:14:26 -0400

    HIVE-7068 Set the necessary info into the AccumuloOutputFormat

--- accumulo-handler/src/java/org/apache/hadoop/hive/accumulo/HiveAccumuloTableOutputFormat.java
+++ accumulo-handler/src/java/org/apache/hadoop/hive/accumulo/HiveAccumuloTableOutputFormat.java
@@ -18,9 +18,15 @@ package org.apache.hadoop.hive.accumulo;
 
 import java.io.IOException;
 
+import org.apache.accumulo.core.client.AccumuloSecurityException;
+import org.apache.accumulo.core.client.ClientConfiguration;
 import org.apache.accumulo.core.client.mapred.AccumuloOutputFormat;
+import org.apache.accumulo.core.client.security.tokens.PasswordToken;
 import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.hive.accumulo.serde.AccumuloSerDeParameters;
 import org.apache.hadoop.mapred.JobConf;
+
+import com.google.common.base.Preconditions;
 
 /**
  * 
@@ -29,7 +35,31 @@ public class HiveAccumuloTableOutputFormat extends AccumuloOutputFormat {
 
   @Override
   public void checkOutputSpecs(FileSystem ignored, JobConf job) throws IOException {
-    System.out.println("");
+    AccumuloConnectionParameters cnxnParams = new AccumuloConnectionParameters(job);
+
+    final String tableName = job.get(AccumuloSerDeParameters.TABLE_NAME);
+
+    // Make sure we actually go the table name
+    Preconditions.checkNotNull(tableName, "Expected Accumulo table name to be provided in job configuration");
+
+    // Set the necessary Accumulo information
+    try {
+      // Username/passwd for Accumulo
+      AccumuloOutputFormat.setConnectorInfo(job, cnxnParams.getAccumuloUserName(),
+          new PasswordToken(cnxnParams.getAccumuloPassword()));
+
+      // Accumulo instance name with ZK quorum 
+      AccumuloOutputFormat.setZooKeeperInstance(job, ClientConfiguration.loadDefault()
+          .withInstance(cnxnParams.getAccumuloInstanceName())
+          .withZkHosts(cnxnParams.getZooKeepers()));
+
+      // Set the table where we're writing this data
+      AccumuloOutputFormat.setDefaultTableName(job, tableName);
+    } catch (AccumuloSecurityException e) {
+      log.error("Could not connect to Accumulo with provided credentials", e);
+      throw new IOException(e);
+    }
+
     super.checkOutputSpecs(ignored, job);
   }
 
